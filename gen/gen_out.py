91eda0b9af38b443ba8ed2a1c76561271bbb07d9
Hi30MC <duimstraash@gmail.com>
2025-07-14 23:08:00 -0400

    experimental wait cutting by using cart reload time algorithm for 27x. needs testing

--- gen/gen_out.py
+++ gen/gen_out.py
@@ -204,6 +204,117 @@ def gen_ROM(door_name: String):
 
     return -1
 
+def gen_ROM_OPTIMIZED(door_name: String):
+    # only for 27x for now
+    # assumes you have enough moves to fill min cart count and there is no terminal wait move
+
+    # generate sequence
+    sequence = gen_ss_sequence(door_name)
+    
+    # get rom metadata
+    door_meta_path = f"door_meta/{door_name}"
+    with open(f"{door_meta_path}/rom_params.txt", "r") as f:
+        rom_params = literal_eval(f.read())
+    
+    # create pos of carts if not specified
+    if "pos" not in rom_params.keys():
+        rom_params.update({"pos": [0.5, 0, 0.5]})
+    
+    meta = SimpleNamespace(**rom_params)
+    
+    # get wait move ss from encoding table
+    ss_encode = get_ss_encode(door_name)
+    wait = ss_encode["wait"]
+
+    # Ensure a couple base params are declared
+    try:
+        _ = meta.density
+    except:
+        raise Exception(f"No value 'density' defined in '{door_meta_path}/rom_params.txt'.")
+    try:
+        _ = meta.min_carts
+    except:
+        raise Exception(f"No value 'min_carts' defined in '{door_meta_path}/rom_params.txt'.")    
+
+    # Ensure "medium" and "min_items_per_cart" keys are defined
+    try:
+        _ = meta.medium
+    except:
+        raise Exception(f"No value 'medium' defined in '{door_meta_path}/rom_params.txt'.")
+    try:
+        _ = meta.min_items_per_cart
+    except:
+        raise Exception(f"No value 'min_items_per_cart' defined in '{door_meta_path}/rom_params.txt'.")
+        
+    # # pad sequence for minimum move count
+    # if len(sequence) < min_items:
+    #     # assign wait move if not already
+    #     if "wait" not in ss_encode.keys():
+    #         wait_backups = [x for x in range(1,16) if x not in ss_encode.values()]
+    #         if len(wait_backups) == 0:
+    #             raise Exception(f"No wait move availible to pad moves. Please create manually.")
+    #         ss_encode.update({"wait": wait_backups[0]})
+    #     # pad to minimum
+    #     if sequence[-1] == 0: # pad waits before the STOP command
+    #         sequence = sequence[:-1] + [ss_encode["wait"]] * (min_items - len(sequence)) + [0]
+    #     else:
+    #         sequence += [ss_encode["wait"]] * (min_items - len(sequence))
+    
+    cart_list = []
+
+    # shitty ass way to one-line splitting a list based on wait move    
+    sequence = [[int(y) for y in x.split()] for x in " ".join([str(x) for x in sequence]).split(str(wait))]
+    queue = []
+    queue += sequence.pop(0)
+    
+    while (len(queue) > 0) or (len(sequence) > 0):
+        if len(queue) <= 27: # entire queue can fit into one cart
+            if len(queue) > min_items_per_cart: # entire quene can fit into one cart and also cover the minimum
+                cart = gen_cart() # gen empty cart
+                for slot in range(len(queue)): # add entire queue to cart slots 0 ... n
+                    ss = queue.pop(0)
+                    if meta.medium == "shulker":
+                        item = gen_ss_sb(slot, ss)
+                    elif meta.medium == "disc":
+                        item = gen_ss_disc(slot, ss)
+                    else:
+                        raise Exception(f"Invalid 'medium' type in {door_meta_path}/rom_params.txt")
+                    add_item_to_cart(cart, item)
+                cart_list.append(cart) # add cart to cart list
+            else: # less than minimum present
+                queue.append(wait)
+                queue += sequence.pop(0)
+            elif len(queue) <= 27 + min_items_per_cart: # in the case(s) that subtracting 27 wouldn't leave enough items in next cart to cover minimum
+                cart = gen(cart) # gen new cart
+                for slot in range(len(queue // 2)): # put first floor(len(queue)/2) items in cart slots 0 ... n
+                    ss = queue.pop(0)
+                    if meta.medium == "shulker":
+                        item = gen_ss_sb(slot, ss)
+                    elif meta.medium == "disc":
+                        item = gen_ss_disc(slot, ss)
+                    else:
+                        raise Exception(f"Invalid 'medium' type in {door_meta_path}/rom_params.txt")
+                    add_item_to_cart(cart, item)
+                cart_list.append(cart) # add cart to cart list
+            else: # safe to pop 27 from queue and have enough to cover minimum of next
+                cart = gen(cart) # gen new cart
+                for slot in range(27): # put first 27 items in cart slots 0 ... 26
+                    ss = queue.pop(0)
+                    if meta.medium == "shulker":
+                        item = gen_ss_sb(slot, ss)
+                    elif meta.medium == "disc":
+                        item = gen_ss_disc(slot, ss)
+                    else:
+                        raise Exception(f"Invalid 'medium' type in {door_meta_path}/rom_params.txt")
+                    add_item_to_cart(cart, item)
+                cart_list.append(cart) # add cart to cart list
+        if (len(queue) == 0) and len(sequence) > 0: # if queue is empty and sequence is not done
+            if sequence[0] == []:
+                queue.append(wait)
+            queue += sequence[0]
+
+    return cart_list
+
 def gen_file(door_name: String, file_name: String):
     door_meta_path = f"door_meta/{door_name}"
     door_gen_path = f"gen/{door_name}"
@@ -228,7 +339,7 @@ if __name__ == "__main__":
     if len(args) == 0:
         print("Pass in door name and file name as two separate parameters. \nLeave off file name to simply use door name.")
     elif len(args) == 1:
-        gen_ROM(*args) # change to gen_file once ready
+        gen_ROM_OPTIMIZED(*args) # change to gen_file once ready
     elif len(args) == 2:
         gen_file(*args)
     else:
